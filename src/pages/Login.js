ffd8a5467abd7292d4d3e6f62bebd060f0d79f8d
marcos <marc0sdurval20@gmail.com>
2022-04-15 14:17:05 -0300

    style: adiciona classes na tela de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../Css/Login.css';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import walletIcon from '../wallet.svg';
 import { recordUser } from '../actions';
 
 class Login extends React.Component {
@@ -18,21 +20,24 @@ class Login extends React.Component {
   }
 
   login(e) {
+    e.preventDefault();
     const { state: { email }, props: { dispatchValue } } = this;
     dispatchValue(email);
     this.setState({ shouldRedirect: false });
-    e.preventDefault();
   }
 
   handleChange({ target }) {
     const { name, value } = target;
-    this.setState({ [name]: value });
-    const { senha, email } = this.state;
-    const vali = /\S+@\S+\.\S+/; /* soure: https://www.horadecodar.com.br/2020/09/13/como-validar-email-com-javascript/ */
-    const num = 5; /* procura por validações de email */
-    if (senha.length >= num && vali.test(email)) {
-      this.setState({ button: false });
-    }
+    this.setState({ [name]: value }, () => {
+      const { senha, email } = this.state;
+      const vali = /\S+@\S+\.\S+/; /* source: https://www.horadecodar.com.br/2020/09/13/como-validar-email-com-javascript/ */
+      const num = 6; /* procura por validações de email */
+      if (senha.length >= num && vali.test(email)) {
+        this.setState({ button: false });
+      } else {
+        this.setState({ button: true });
+      }
+    });
   }
 
   render() {
@@ -41,31 +46,39 @@ class Login extends React.Component {
       return <Redirect to="/carteira" />;
     }
     return (
-      <form onSubmit={ this.login }>
-        <div>Login</div>
-        <label htmlFor="login">
-          E-mail:
-          <input
-            onChange={ this.handleChange }
-            type="email"
-            name="email"
-            required
-            data-testid="email-input"
-          />
-        </label>
-        <label htmlFor="senha">
-          Senha:
-          <input
-            type="text"
-            required
-            minLength="6"
-            name="senha"
-            onChange={ this.handleChange }
-            data-testid="password-input"
-          />
-        </label>
-        <button type="submit" disabled={ button }>Entrar</button>
-      </form>
+      <div className="pageLogin">
+        <form className="login" onSubmit={ this.login }>
+          <img src={ walletIcon } className="icon" alt="" />
+          <div className="form-group">
+            <label htmlFor="login">
+              E-mail
+              <input
+                onChange={ this.handleChange }
+                type="email"
+                name="email"
+                required
+                data-testid="email-input"
+                className="form-control"
+              />
+            </label>
+          </div>
+          <div className="form-group">
+            <label htmlFor="senha">
+              Senha
+              <input
+                type="text"
+                required
+                minLength="6"
+                name="senha"
+                onChange={ this.handleChange }
+                data-testid="password-input"
+                className="form-control"
+              />
+            </label>
+          </div>
+          <button type="submit" className="btn-login" disabled={ button }>Entrar</button>
+        </form>
+      </div>
     );
   }
 }
@@ -75,6 +88,5 @@ const mapDispatchToProps = (dispatch) => ({
 
 Login.propTypes = {
   dispatchValue: PropTypes.func.isRequired,
-  // history: PropTypes.objectOf.isRequired,
 };
 export default connect(null, mapDispatchToProps)(Login);
