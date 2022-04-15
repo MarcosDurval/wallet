2a160f0f293bf453b8bec8fbed7b496a5f385e9d
marcos <marc0sdurval20@gmail.com>
2022-04-15 14:13:10 -0300

    refactor:  remove comentarios da trybe

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,6 @@
-// import user from './user';
 import { combineReducers } from 'redux';
 import user from './user';
-// import wallet from './wallet';
 import wallet from './wallet';
-// Configure os seus reducers.
-// ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
 
 const rootreducer = combineReducers({ user, wallet });
 
